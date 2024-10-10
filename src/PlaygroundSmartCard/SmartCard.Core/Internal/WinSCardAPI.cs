305d2f74c0b76665b18904922109ba2fc66bce17
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-10 16:44:01 +0600

    Add WinSCardAPI class for internal use

---
+++ src/PlaygroundSmartCard/SmartCard.Core/Internal/WinSCardAPI.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartCard.Core.Internal
+{
+    /// <summary>
+    /// A class that wraps the functionality of winscard.dll and its methods.
+    /// This class is internal to prevent external access to the SmartCard API.
+    /// </summary>
+    internal class WinSCardAPI
+    {
+    }
+}
