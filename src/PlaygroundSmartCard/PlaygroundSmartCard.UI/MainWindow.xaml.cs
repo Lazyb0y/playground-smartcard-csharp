8b1385778945a4ab83cea4945f7023777e3b6259
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-10 13:45:44 +0600

    Add WPF project for UI

---
+++ src/PlaygroundSmartCard/PlaygroundSmartCard.UI/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PlaygroundSmartCard.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
