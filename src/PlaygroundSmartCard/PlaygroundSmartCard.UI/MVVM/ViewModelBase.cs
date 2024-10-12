1483983ea4912d428c115e83c6274da0a3417a52
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-12 13:43:30 +0600

    Add MVVM related classes

---
+++ src/PlaygroundSmartCard/PlaygroundSmartCard.UI/MVVM/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PlaygroundSmartCard.UI.MVVM
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
