912db739c0a0906aa575ac2eac45e0454e113bd4
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-17 22:45:57 +0600

    Add ATR class to represents an Answer To Reset (ATR) of a smart card and an enum to represent card type

---
+++ src/PlaygroundSmartCard/SmartCard.Core/ATR.cs
@@ -0,0 +1,79 @@
+﻿namespace SmartCard.Core
+{
+    /// <summary>
+    /// Represents an Answer To Reset (ATR) of a smart card.
+    /// </summary>
+    public class ATR
+    {
+        #region Declaration(s)
+
+        private readonly string _sanitizedATRString;
+
+        #endregion
+
+        #region Property(s)
+
+        /// <summary>
+        /// Gets the original ATR string.
+        /// </summary>
+        public string ATRString { get; }
+
+        #endregion
+
+        #region Constructor(s)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ATR"/> class with the specified ATR string.
+        /// </summary>
+        /// <param name="atrString">The ATR string.</param>
+        public ATR(string atrString)
+        {
+            ATRString = atrString;
+
+            _sanitizedATRString = atrString
+                .Replace(" ", string.Empty)
+                .Replace("-", string.Empty)
+                .ToUpper();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets the type of the smart card based on the ATR string.
+        /// </summary>
+        /// <returns>The type of the smart card.</returns>
+        public SmartCardType GetCardType()
+        {
+            if (_sanitizedATRString.StartsWith("3B65"))
+            {
+                return SmartCardType.EMV;
+            }
+
+            if (_sanitizedATRString.StartsWith("3B8F80"))
+            {
+                return SmartCardType.Mifare;
+            }
+
+            if (_sanitizedATRString.StartsWith("3B3F11008012009131C0640E0146AC72F74105"))
+            {
+                return SmartCardType.Scosta;
+            }
+
+            if (_sanitizedATRString.StartsWith("3B9F"))
+            {
+                return SmartCardType.SIM;
+            }
+
+            return SmartCardType.Unknown;
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current ATR object.
+        /// </summary>
+        /// <returns>A string that represents the current ATR object.</returns>
+        public override string ToString()
+        {
+            return ATRString;
+        }
+    }
+}
---
+++ src/PlaygroundSmartCard/SmartCard.Core/SmartCardType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace SmartCard.Core
+{
+    /// <summary>
+    /// Represents the type of smart card.
+    /// </summary>
+    [DefaultValue(Unknown)]
+    public enum SmartCardType
+    {
+        Unknown,
+        EMV,
+        Mifare,
+        Scosta,
+        SIM
+    }
+}
