c4a35c1130bed6f94806bd3c08072c20caa26532
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-17 12:37:31 +0600

    Update WinSCardScope and WinSCardState struct

--- src/PlaygroundSmartCard/SmartCard.Core/Internal/WinSCardScope.cs
+++ src/PlaygroundSmartCard/SmartCard.Core/Internal/WinSCardScope.cs
@@ -5,19 +5,26 @@
     /// </summary>
     internal struct WinSCardScope
     {
+        // ReSharper disable InconsistentNaming
+
         /// <summary>
         /// The scope is limited to the current user.
         /// </summary>
-        public const uint SCARD_SCOPE_USER = 0;
+        public const uint SCARD_SCOPE_USER = 0x00000000;
 
         /// <summary>
         /// The scope is limited to the current terminal.
         /// </summary>
-        public const uint SCARD_SCOPE_TERMINAL = 1;
+        public const uint SCARD_SCOPE_TERMINAL = 0x00000001;
 
         /// <summary>
         /// The scope is limited to the system.
         /// </summary>
-        public const uint SCARD_SCOPE_SYSTEM = 2;
+        public const uint SCARD_SCOPE_SYSTEM = 0x00000002;
+
+        /// <summary>
+        /// The scope is global.
+        /// </summary>
+        public const uint SCARD_SCOPE_GLOBAL = 0x00000003;
     }
 }
--- src/PlaygroundSmartCard/SmartCard.Core/Internal/WinSCardState.cs
+++ src/PlaygroundSmartCard/SmartCard.Core/Internal/WinSCardState.cs
@@ -13,9 +13,24 @@
         public const uint SCARD_STATE_UNAWARE = 0x00000000;
 
         /// <summary>
+        /// The ignore state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_IGNORE = 0x00000001;
+
+        /// <summary>
         /// The changed state of a WinSCard.
         /// </summary>
         public const uint SCARD_STATE_CHANGED = 0x00000002;
+
+        /// <summary>
+        /// The unknown state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_UNKNOWN = 0x00000004;
+
+        /// <summary>
+        /// The unavailable state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_UNAVAILABLE = 0x00000008;
 
         /// <summary>
         /// The not present state of a WinSCard.
@@ -26,5 +41,30 @@
         /// The present state of a WinSCard.
         /// </summary>
         public const uint SCARD_STATE_PRESENT = 0x00000020;
+
+        /// <summary>
+        /// The ATR match state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_ATRMATCH = 0x00000040;
+
+        /// <summary>
+        /// The exclusive state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_EXCLUSIVE = 0x00000080;
+
+        /// <summary>
+        /// The in use state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_INUSE = 0x00000100;
+
+        /// <summary>
+        /// The mute state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_MUTE = 0x00000200;
+
+        /// <summary>
+        /// The unpowered state of a WinSCard.
+        /// </summary>
+        public const uint SCARD_STATE_UNPOWERED = 0x00000400;
     }
 }
